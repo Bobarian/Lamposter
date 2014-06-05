dfe0fd53ad1fc6d7b918076aa8efcf77ad6aebe8
Bob Schoenthal <BSchoenthal@gmail.com>
2014-06-05 14:32:45 -0700

    Initial push of Lamposter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lamposter::Application.config.secret_key_base = '6eb960cad26f7f1f902b50b4de535375b171420868d9634ab296ad24b55f996cfe26bf20327a73e7c003cce904d1f367174590a60286d7a9a81d7fc6b381e886'
